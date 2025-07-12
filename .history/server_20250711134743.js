a3da55bb9acee37038fee7a4d8b20ab73f20cdb4
anthony <ayomideobaroovooboh@email.com>
2025-07-12 18:53:07 +0100

    preparing app for deployment

--- .history/server_20250711134743.js
+++ .history/server_20250711134743.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  //console.log(err.name, err.message);
   process.exit(1);
 });
 
@@ -15,13 +16,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD
 );
 
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log('DB connection successful!'));
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
@@ -30,7 +25,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  //console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
