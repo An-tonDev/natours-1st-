69babcf06ea2e32acf0c6b88ed07f7555a878046
anthony <ayomideobaroovooboh@email.com>
2025-07-16 13:03:23 +0100

    webhooks srtipen

--- .history/controllers/bookingController_20250716130220.js
+++ .history/controllers/bookingController_20250716130220.js
@@ -7,53 +7,6 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('../controllers/handlerFactory');
 const User = require('../models/userModel');
 
-exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.tourId);
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    customer_email: req.user.email,
-    client_reference_id: req.params.tourId,
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          unit_amount: tour.price * 100, // Stripe expects amount in cents
-          product_data: {
-            name: `${tour.name} Tour`,
-            description: tour.summary,
-            images: [
-              `${req.protocol}://${req.get('host')}/img/tours/${
-                tour.imageCover
-              }`
-            ]
-          }
-        },
-        quantity: 1
-      }
-    ],
-    mode: 'payment'
-  });
-
-  res.status(200).json({
-    status: 'success',
-    session
-  });
-});
-
-/* exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  const { tour, user, price } = req.query;
-
-  if (!tour || !user || !price) {
-    return next();
-  }
-
-  await Booking.create({ tour, user, price });
-
-  res.redirect(req.originalUrl.split('?')[0]);
-}); */
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
